6b7407c90262ab14cc9e7651d718d098fe6a155c
JulioJacinto890 <124766374+JulioJacinto890@users.noreply.github.com>
2023-02-28 00:59:46 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE.2/src/eva1_14_alcance/pkg2/EVA1_14_ALCANCE2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_14_alcance.pkg2;
+
+/**
+ *
+ * @author hp
+ */
+public class EVA1_14_ALCANCE2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 100;
+        for (int i = 0; i < 10; i++) {
+            int x =100;
+            
+            int y = 100;//visible dentro del for
+            if (i == 5){
+                
+            }
+            
+        }
+        int y = 100; // // fuera del bloque no exist 
+    }
+    
+}
